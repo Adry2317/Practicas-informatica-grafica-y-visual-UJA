537f7950ec539be009bde8b5012c861aecb8b453
aaf00022 <aaf00022@red.ujaen.es>
2021-11-04 11:16:30 +0100

    pr3b inicio

---
+++ Practica3b_IGV/igvEscena3D.cpp
@@ -0,0 +1,79 @@
+#include <cstdlib>
+#include <stdio.h>
+
+#include "igvEscena3D.h"
+
+// Metodos constructores 
+
+igvEscena3D::igvEscena3D() {
+	ejes = true;
+
+	// Apartado C: inicializar los atributos para el control de los grados de libertad del modelo 
+
+}
+
+igvEscena3D::~igvEscena3D() {
+
+}
+
+
+// Metodos publicos 
+
+void pintar_ejes(void) {
+	GLfloat rojo[] = { 1,0,0,1.0 };
+	GLfloat verde[] = { 0,1,0,1.0 };
+	GLfloat azul[] = { 0,0,1,1.0 };
+
+	glMaterialfv(GL_FRONT, GL_EMISSION, rojo);
+	glBegin(GL_LINES);
+	glVertex3f(1000, 0, 0);
+	glVertex3f(-1000, 0, 0);
+	glEnd();
+
+	glMaterialfv(GL_FRONT, GL_EMISSION, verde);
+	glBegin(GL_LINES);
+	glVertex3f(0, 1000, 0);
+	glVertex3f(0, -1000, 0);
+	glEnd();
+
+	glMaterialfv(GL_FRONT, GL_EMISSION, azul);
+	glBegin(GL_LINES);
+	glVertex3f(0, 0, 1000);
+	glVertex3f(0, 0, -1000);
+	glEnd();
+}
+
+///// Apartado B: Métodos para visualizar cada parte del modelo
+
+////// Apartado C: añadir aquí los métodos para modificar los grados de libertad del modelo
+
+
+void igvEscena3D::visualizar() {
+	// crear luces
+	GLfloat luz0[4] = { 5.0,5.0,5.0,1 }; // luz puntual  
+	glLightfv(GL_LIGHT0, GL_POSITION, luz0); // la luz se coloca aquí si permanece fija y no se mueve con la escena
+	glEnable(GL_LIGHT0);
+
+	// crear el modelo
+	glPushMatrix(); // guarda la matriz de modelado
+
+	// se pintan los ejes
+	if (ejes) pintar_ejes();
+
+	//glLightfv(GL_LIGHT0,GL_POSITION,luz0); // la luz se coloca aquí si se mueve junto con la escena (también habría que desactivar la de arriba).
+
+
+
+	///// Apartado B: aquí hay que añadir la visualización del árbol del modelo utilizando la pila de matrices de OpenGL
+	/////             se recomienda crear una método auxiliar que encapsule todo el código para la visualización
+	/////             del modelo, dejando aquí sólo la llamada a ese método, así como distintas funciones una para cada
+	/////			  parte del modelo. 
+
+
+
+	glPopMatrix(); // restaura la matriz de modelado
+}
+
+
+
+
